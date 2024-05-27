1a729d3c6333bd7fbf38a3412ba23f67368de440
Martial <floquet.martial@gmail.com>
2024-05-27 15:46:12 +0200

    Generator ending

--- cccop.cbl
+++ cccop.cbl
@@ -1,8 +1,6 @@
-  
-      ****************************************************************** 
        IDENTIFICATION DIVISION.
        PROGRAM-ID. cccop.
-      
+       AUTHOR.  AlexEtRemi.
 
       ******************************************************************
        ENVIRONMENT DIVISION.
@@ -21,18 +19,23 @@
 
       ******************************************************************
        DATA DIVISION.
+      ******************************************************************
+
        FILE SECTION.
        FD  F-INPUT
-           RECORD CONTAINS 230 CHARACTERS
-           RECORDING MODE IS F.
-       01  R-INPUT PIC X(230).
+           RECORD CONTAINS 5 TO 1000 CHARACTERS
+           RECORDING MODE IS V.
+       01  R-INPUT PIC X(215).
 
        FD  F-OUTPUT
            RECORD CONTAINS 80 CHARACTERS
            RECORDING MODE IS F.
        01  R-OUTPUT PIC X(80).
 
+      ******************************************************************
        WORKING-STORAGE SECTION.
+      ******************************************************************
+
        01  FS-INPUT PIC X(02).
            88 FS-INPUT-OK VALUE "00".
            88 FS-INPUT-EOF VALUE "10".
@@ -40,52 +43,48 @@
        01  FS-OUTPUT PIC X(02).
            88 FS-OUTPUT-OK VALUE "00".
 
-       01  GROUPE.
-           03 CNT PIC 9(04) VALUE 0.
-           03 ARRAY OCCURS 1 TO 1000 TIMES
-                     DEPENDING ON CNT
-                     INDEXED BY IDX.
-               05 VALEUR PIC X(20).
+       01  WS-ALL.
+           03 WS-COUNTER PIC 9(04) VALUE 1.
+           03 WS-ARRAY OCCURS 1 TO 1000 TIMES
+                     DEPENDING ON WS-COUNTER
+                     INDEXED BY GROUPE-IDX.
+               05 WS-VALUE PIC X(20).
 
-       01  VAR PIC 9(02) VALUE 1.
-       01  WS-IDX PIC 9(2).
-       01  WS-IDX2 PIC 9(2).
-       01  LONGUEUR pic 99.
-       01  CNT-SPACE pic 99.
+       01  TEMP                  PIC 9(02) VALUE 1.
+       01  WS-INDEX             PIC 9(03).
+       01  WS-INDEX-2           PIC 9(03) VALUE 1.
+       
 
       ****************************************************************** 
        PROCEDURE DIVISION.
+      ******************************************************************      
+      
        START-MAIN.
-           PERFORM START-R-IP THRU END-R-IP.
+           PERFORM 1000-READ.
        END-MAIN.
            STOP RUN.
 
       ******************************************************************
-       START-R-IP.
+       1000-READ.
            OPEN INPUT F-INPUT
                 OUTPUT F-OUTPUT.
+                CLOSE  F-OUTPUT.
+           OPEN EXTEND F-OUTPUT.
+              
+              
+              
+              SET TEMP TO 0.
            IF FS-INPUT EQUAL "00"
               SET FS-INPUT-OK TO TRUE
-               
+                    MOVE "       01 GROUPE." TO R-OUTPUT
+                    WRITE R-OUTPUT
               PERFORM UNTIL FS-INPUT-EOF
                  READ F-INPUT 
                  AT END 
                     SET FS-INPUT-EOF TO TRUE
                  NOT AT END 
-                 MOVE "       01 GROUPE." TO R-OUTPUT
-                 WRITE R-OUTPUT
-                 ADD 1 TO WS-IDX
-                 IF WS-IDX < 21 THEN
-                  UNSTRING R-INPUT DELIMITED BY SPACE INTO R-OUTPUT
-                  END-UNSTRING
-                 
-                 COMPUTE VAR = WS-IDX - 1
-                 STRING "           03  FILLER PIC X(", 
-                    VAR, ") VALUE ", "'", ARRAY(WS-IDX), "'" "."
-                    DELIMITED BY SPACE
-                    INTO R-OUTPUT
-                  END-STRING
-                    WRITE R-OUTPUT
+
+                    PERFORM 2000-WRITE 
                     
                   END-READ
               END-PERFORM
@@ -93,20 +92,44 @@
               DISPLAY "ERREUR :" SPACE FS-INPUT
            END-IF.
            CLOSE F-INPUT.
-       END-R-IP.
+           CLOSE  F-OUTPUT.
            EXIT.
 
-      ******************************************************************
-       START-W-OP.
+      *-----------------------------------------------------------------
+       2000-WRITE.
 
-           INSPECT R-INPUT TALLYING CNT-SPACE FOR ALL " "
-           PERFORM VARYING WS-IDX FROM 1 BY 1 UNTIL WS-IDX > 
-           LENGTH OF ARRAY(WS-IDX)
-           ADD 1 TO CNT
-           END-PERFORM
-          
-           DISPLAY CNT
-           WRITE R-OUTPUT.
-       END-W-OP.
+            PERFORM VARYING WS-INDEX FROM 1 BY 1 UNTIL
+             WS-INDEX > LENGTH OF R-INPUT
+            SET TEMP TO 0
+               IF R-INPUT(WS-INDEX:1) = SPACE
+                 PERFORM VARYING WS-INDEX 
+                    FROM WS-INDEX BY 1 UNTIL 
+                    R-INPUT(WS-INDEX:1) NOT EQUAL SPACE
+                    ADD 1 TO TEMP
+                    END-PERFORM                    
+                    STRING "           03  FILLER PIC X(", 
+                    TEMP, ")." 
+                    DELIMITED BY SIZE
+                    INTO R-OUTPUT
+                    WRITE R-OUTPUT
+                    INITIALIZE   R-OUTPUT
+               ELSE
+                    MOVE 0 TO TEMP
+                    PERFORM VARYING WS-INDEX 
+                    FROM WS-INDEX BY 1 UNTIL 
+                    R-INPUT(WS-INDEX:1) = SPACE
+                    OR WS-INDEX > LENGTH OF R-INPUT
+                    ADD 1 TO TEMP
+            END-PERFORM
+                    STRING "           03  FILLER PIC X(", 
+                    TEMP, ") VALUE ", R-INPUT(WS-INDEX-2:TEMP)  , "." 
+                    DELIMITED BY SIZE
+                    INTO R-OUTPUT
+                    WRITE R-OUTPUT
+                    INITIALIZE   R-OUTPUT
+               END-IF
+                    SUBTRACT 1 FROM WS-INDEX
+            ADD TEMP to WS-INDEX-2           
+           END-PERFORM.
            EXIT.
           
